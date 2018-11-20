218c3e5c42e3244323fcce5d76e542e18e316754
Roms <romelyn.ungab@ipfx.com>
2018-11-21 07:56:06 +1300

    intialized with boilerplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import 'normalize.css/normalize.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boilerplater</p>, document.getElementById('app'));
