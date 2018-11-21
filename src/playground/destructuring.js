d5a146eebcc0612b0bea921608141b20129c194e
Roms <romelyn.ungab@ipfx.com>
2018-11-22 12:10:24 +1300

    using redux store, reducer, action generator

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -19,15 +19,16 @@
 //     console.log(`it's ${temperature} in ${city}`)
 // }
 
-const book = {
-    title:'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher:{
-        name: 'Penguin'
-    }
-};
-
-const {name:publisherName = 'Self-published'} = book.publisher;
-console.log(publisherName)
 
 //ARRAY destructuring
+
+const address = ['1299 S Juniper Street', 'Philadelphia','Pennsylvania','19147'];
+
+const [,city, state='New York',zip] = address;
+console.log(`You are in ${city}, ${state}`);
+
+
+const item =['Coffee (iced)','$3.00','$3.50', '3.75'];
+
+const [itemName, ,medium] = item;
+console.log(`A medium ${itemName} costs ${medium}`)
